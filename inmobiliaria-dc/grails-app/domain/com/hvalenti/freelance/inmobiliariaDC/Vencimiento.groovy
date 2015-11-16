7addb89f698fcf6f2f1dcdc4e053a17c2021dc9a
heril <heraldovalenti@gmail.com>
2015-11-16 12:39:49 -0300

    added esPendiente() to vencimiento and reference to liquidacion

--- inmobiliaria-dc/grails-app/domain/com/hvalenti/freelance/inmobiliariaDC/Vencimiento.groovy
+++ inmobiliaria-dc/grails-app/domain/com/hvalenti/freelance/inmobiliariaDC/Vencimiento.groovy
@@ -5,26 +5,34 @@ class Vencimiento {
 
 	Date vencimiento
 	Double monto
+	DetalleLiquidacion liquidacion
 
 	static belongsTo = [responsableObligacion: ResponsableObligacion]
 
 	static constraints = {
+		liquidacion nullable: true
+	}
+	
+	public boolean esPendiente(Date now) {
+		return ( liquidacion == null && ( vencimiento.equals(now) || vencimiento.before(now) ))
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
+		if (obj == null) {
+			return false
+		}
+		if (getClass() != obj.getClass()) {
+			return false
+		}
 		Vencimiento other = (Vencimiento) obj;
 		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+			if (other.id != null) {
+				return false
+			}
+		} else if (!id.equals(other.id)) {
+			return false
+		}
+		return true
 	}
 }
---
+++ inmobiliaria-dc/test/unit/com/hvalenti/freelance/inmobiliariaDC/VencimientoSpec.groovy
@@ -0,0 +1,68 @@
+
+package com.hvalenti.freelance.inmobiliariaDC
+
+import com.hvalenti.freelance.inmobiliariaDC.util.DateUtil;
+
+import spock.lang.Specification;
+
+class VencimientoSpec extends Specification {
+	
+	def "vencimiento no es pendiente cuando tiene liquidacion asociada"() {
+		given:
+		Date now = DateUtil.dateFromString("2015-06-01")
+		Vencimiento v1 = new Vencimiento()
+		v1.vencimiento = now
+		v1.liquidacion = new DetalleLiquidacion()
+		
+		when:
+		boolean pendiente = v1.esPendiente(now)
+		
+		then:
+		pendiente == false
+	}
+	
+	def "vencimiento no es pendiente cuando la fecha es anterior al vencimiento"() {
+		given:
+		Date fechaVencimiento = DateUtil.dateFromString("2015-06-02")
+		Date now = DateUtil.dateFromString("2015-06-01")
+		Vencimiento v1 = new Vencimiento()
+		v1.vencimiento = fechaVencimiento
+		v1.liquidacion = null
+		
+		when:
+		boolean pendiente = v1.esPendiente(now)
+		
+		then:
+		pendiente == false
+	}
+	
+	def "vencimiento es pendiente cuando la fecha es igual al vencimiento"() {
+		given:
+		Date now = DateUtil.dateFromString("2015-06-01")
+		Vencimiento v1 = new Vencimiento()
+		v1.vencimiento = now
+		v1.liquidacion = null
+		
+		when:
+		boolean pendiente = v1.esPendiente(now)
+		
+		then:
+		pendiente == true
+	}
+	
+	def "vencimiento es pendiente cuando la fecha es despues al vencimiento"() {
+		given:
+		Date fechaVencimiento = DateUtil.dateFromString("2015-06-01")
+		Date now = DateUtil.dateFromString("2015-06-02")
+		Vencimiento v1 = new Vencimiento()
+		v1.vencimiento = fechaVencimiento
+		v1.liquidacion = null
+		
+		when:
+		boolean pendiente = v1.esPendiente(now)
+		
+		then:
+		pendiente == true
+	}
+
+}
