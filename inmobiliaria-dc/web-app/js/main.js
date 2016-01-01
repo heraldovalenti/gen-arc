8590c4f48670c621d31c88259c52cf33c03a6cf9
heril <heraldovalenti@gmail.com>
2015-12-31 22:26:18 -0300

    fixed js dependencies and loaded page successfully

--- inmobiliaria-dc/web-app/js/main.js
+++ inmobiliaria-dc/web-app/js/main.js
@@ -1,19 +1,34 @@
-requirejs([
-           "template/jquery.min",
-           "template/bootstrap.min",
-           "template/custom",
-           //"template/chartjs/chart.min",
-           //"template/progressbar/bootstrap-progressbar.min",
-           //"template/nicescroll/jquery.nicescroll.min"
-           ], function() {
-	
+requirejs.config({
+    baseUrl : './js',
+    shim : {
+
+        bootstrap : {
+            deps : [ 'jquery'],
+            exports: 'Bootstrap'
+        },
+        
+        nicescroll : {
+        	deps : ['jquery']
+        },
+        
+        custom : {
+        	deps : ['jquery', 'bootstrap', 'nicescroll']
+        }
+    },
+
+    paths : {
+        jquery : 'template/jquery.min',
+        bootstrap : 'template/bootstrap.min',
+        custom : 'template/custom',
+        chartjs : 'template/chartjs/chart.min',
+        progressbar : 'template/progressbar/bootstrap-progressbar.min',
+        nicescroll : 'template/nicescroll/jquery.nicescroll.min',
+        icheck : 'template/icheck/icheck.min',
+        moris : 'template/moris/raphael-min'
+    }
 });
 
-/*
- * 
-,
 
-
-"template/icheck/icheck.min",
-
-"template/moris/raphael-min"],*/
+requirejs(["custom"], function(custom) {
+	
+});
