728cc2adf8fb0d8aa5f25ccb529a457806a7a0bd
heral <heral@Heril-NB>
2016-04-24 19:05:27 -0300

    added alternatives datasources

--- inmobiliaria-dc/grails-app/conf/DataSource.groovy
+++ inmobiliaria-dc/grails-app/conf/DataSource.groovy
@@ -5,3 +5,34 @@ dataSource {
     username = ""
     password = ""
 }
+
+environments {
+    development { //derby database
+        dataSource {
+			dbCreate = "update"
+		    driverClassName = "org.apache.derby.jdbc.ClientDriver"
+			url = "jdbc:derby://localhost:1527/inmobiliaria;create=true"
+		    username = ""
+		    password = ""
+        }
+    }
+    test { //a in-memory database
+        dataSource {
+            dbCreate = "update"
+		    pooled = true
+		    jmxExport = true
+		    driverClassName = "org.h2.Driver"
+		    username = "sa"
+		    password = ""
+        }
+    }
+    production { // PostgreSQL DB - equivalent to the one on OpenShift
+        dataSource {
+			dbCreate = "update"
+		    driverClassName = "org.postgresql.Driver"
+			url = "jdbc:postgresql://localhost:5432/app1"
+		    username = "admin9u9qsdb"
+		    password = "Q9d2HsMZVpKh"
+        }
+    }
+}
